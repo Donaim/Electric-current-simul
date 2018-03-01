ecfa9b23ae141bdcd8cdf4e399ed2549b02c5eed
d0naim <d0naim@jerrs.dsnet.umcs.pl>
2018-03-01 20:32:22 +0100

    release 1.0 ?

--- main.cpp
+++ main.cpp
@@ -10,20 +10,6 @@
 
 using namespace std;
 
-void test_list() {
-    SList<char> t{1};
-    t.push_back('A');
-    t.push_back('B');
-    t.push_back('\0');
-    cout << '\"' << t.source() << '\"' << endl;
-    t.forget_and_alloc_new(2);
-    t.push_back('X');
-    t.push_back('Y');
-    t.push_back('6');
-    t.push_back('\0');
-    cout << '\"' << t.source() << '\"' << endl;
-}
-
 int main() {
     srand(time(0));
 
