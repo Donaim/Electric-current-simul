0d33c0a23e735e7797ef355aa78f85ab69d4f2f3
d0naim <d0naim@jerrs.dsnet.umcs.pl>
2018-03-01 19:37:45 +0100

    tree-like connector is MUCH faster than intuitive. Great work :)

--- main.cpp
+++ main.cpp
@@ -27,16 +27,16 @@ void test_list() {
 int main() {
     srand(time(0));
 
-    auto sh = *new RectangleF{0, 0, 10, 10};
-    // auto p = *new DensityParams{sh, connectors::Intuitive{}, 2.1f};
-    auto p = *new DensityParams{sh, connectors::TreeLike{}, 2.1f};
+    auto sh = *new RectangleF{0, 0, 1, 1};
+    // auto p = *new DensityParams{sh, connectors::Intuitive{}, 100000.0};
+    auto p = *new DensityParams{sh, connectors::TreeLike{}, 100000.0};
     ConnectedNetwork nt = SimpleCreator{}.create_solid_random_network(p);
     cout << "network connected!" << endl;
 
-    cout << nt << " : " << nt.total_charge() << endl;
-    nt.lap();
+    // cout << nt << " : " << nt.total_charge() << endl;
+    // nt.lap();
     cout << "lap finished" << endl;
-    cout << nt << " : " << nt.total_charge() << endl;
+    // cout << nt << " : " << nt.total_charge() << endl;
 
     cout << "end" << endl;
     return 0;
